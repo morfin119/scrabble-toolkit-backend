5fa30a466e5b2d633cf8c32ef4c32feaad5d4ad4
Ricardo Morfin <rmorfin119@gmail.com>
2024-04-22 11:07:56 -0600

    Changes: - Add morgan dependency. - Add language endpoint.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
 const configureSwagger = require('./config/swagger.config');
 const initializeServices = require('./config/services.config');
+const languageRoutes = require('./language/language.routes');
 
 /**
  * Starts the sever by listening on the specified port or a default port if not
@@ -16,14 +18,20 @@ function startServer (app) {
   });
 }
 
-// Create an instance of an express app
+// Create an instance of an express app.
 const app = express();
 
-// Configure Swagger documentation
+// Configure Swagger documentation.
 configureSwagger(app);
 
-// Initialize services
+// Register morgan middleware.
+app.use(morgan('tiny'));
+
+// Initialize services.
 initializeServices();
 
-// Start the server
+// Define routes.
+app.use('/api/languages', languageRoutes);
+
+// Start the server.
 startServer(app);
