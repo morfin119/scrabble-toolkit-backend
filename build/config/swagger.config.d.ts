85c5c4e1dd562d78a6837b06aa757329c433418a
Ricardo Morfin <rmorfin119@gmail.com>
2024-05-06 13:45:10 -0600

    Update .gitignore for TypeScript rules.

--- build/config/swagger.config.d.ts
+++
@@ -1,8 +0,0 @@
-import { Express } from 'express';
-/**
- * Configures Swagger for the application.
- * Sets up Swagger UI middleware to serve API documentation.
- * @param {Express} app Express application instance.
- * @returns {void}
- */
-export default function configureSwagger(app: Express): void;
