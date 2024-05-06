3bd14873f438b287eceb479f96fda22c0718c3fb
Ricardo Morfin <rmorfin119@gmail.com>
2024-05-06 13:32:42 -0600

    Switch to TypeScript, integrate Google's TypeScript style guide (GTS).

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
-const express = require('express');
-const morgan = require('morgan');
-const configureSwagger = require('./config/swagger.config');
+import express, {Express} from 'express';
+import morgan from 'morgan';
+import configureSwagger from '@config/swagger.config';
 
 /**
  * Starts the sever by listening on the specified port or a default port if not
  * provided.
  *
- * @param {Express} app The express app instance to start the server with.
- * @returns {void}
+ * @param app
+ * The express app instance to start the server with.
+ * @returns
  */
-function startServer (app) {
+function startServer(app: Express): void {
   const port = process.env.PORT || 3000;
   app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
@@ -17,7 +18,7 @@ function startServer (app) {
 }
 
 // Create an instance of an express app
-const app = express();
+const app: Express = express();
 
 // Configure Swagger documentation
 configureSwagger(app);
--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,26 +1,26 @@
-const swaggerUi = require('swagger-ui-express');
-const swaggerJSDoc = require('swagger-jsdoc');
+import {Express} from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from 'swagger-jsdoc';
 
-const swaggerOptions = {
+const swaggerOptions: swaggerJSDoc.Options = {
   definition: {
     openapi: '3.1.0',
     info: {
       title: 'Scrabble Toolkit RESTful API',
-      version: '0.1.0'
-    }
+      version: '0.1.0',
+    },
   },
-  apis: ['./src/**/*.js']
+  apis: ['./src/**/*.js', './src/**/*.ts'],
 };
 
 /**
  * Configures Swagger for the application.
  * Sets up Swagger UI middleware to serve API documentation.
- *
  * @param {Express} app Express application instance.
  * @returns {void}
  */
-function configureSwagger (app) {
-  // Initialize swagger-jsdoc -> returns validated swagger spec in json format
+export default function configureSwagger(app: Express) {
+  // Initialize swagger-jsdoc -> returns validated swagger spec in json format.
   const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
   // Configure Swagger UI middleware
@@ -28,5 +28,3 @@ function configureSwagger (app) {
 
   console.log('API documentation available at {root_url}/api-docs');
 }
-
-module.exports = configureSwagger;
