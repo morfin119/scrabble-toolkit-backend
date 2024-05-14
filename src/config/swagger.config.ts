55f8af3575bf08977ec531d4265956d943b58f6f
Ricardo Morfin <rmorfin119@gmail.com>
2024-05-14 12:00:25 -0600

    Refactor Swagger documentation: Migrated from swagger-jsdoc to JSON file structure.

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,30 +1,13 @@
 import {Express} from 'express';
 import swaggerUi from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
-
-const swaggerOptions: swaggerJSDoc.Options = {
-  definition: {
-    openapi: '3.1.0',
-    info: {
-      title: 'Scrabble Toolkit RESTful API',
-      version: '0.1.0',
-    },
-  },
-  apis: ['./src/**/*.js', './src/**/*.ts'],
-};
+import swaggerDocument from '@src/docs/swagger.json';
 
 /**
  * Configures Swagger for the application.
  * Sets up Swagger UI middleware to serve API documentation.
- * @param {Express} app Express application instance.
- * @returns {void}
+ * @param app Express application instance.
  */
 export default function configureSwagger(app: Express) {
-  // Initialize swagger-jsdoc -> returns validated swagger spec in json format.
-  const swaggerSpec = swaggerJSDoc(swaggerOptions);
-
-  // Configure Swagger UI middleware
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   console.log('API documentation available at {root_url}/api-docs');
 }
