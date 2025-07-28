f23b043bbe658e3d82c78fe105f5917ee6a47aaa
Ricardo Morfín <rmorfin119@gmail.com>
2025-07-27 19:41:51 -0700

    fix: add try/catch blocks to TileSetController for error handling

--- src/components/TileSet/TileSet.controller.ts
+++ src/components/TileSet/TileSet.controller.ts
@@ -1,4 +1,4 @@
-import {Router, Request, Response} from 'express';
+import {Router, Request, Response, NextFunction} from 'express';
 import {autoInjectable} from 'tsyringe';
 import {param, validationResult} from 'express-validator';
 import TileSetService from '@components/TileSet/TileSet.service';
@@ -14,7 +14,13 @@ const validateLanguage = [
 
 // Middleware for name validation
 const validateName = [
-  param('name').exists().withMessage('Name parameter is required.'),
+  param('name')
+    .exists()
+    .withMessage('Name parameter is required.')
+    .toUpperCase()
+    .isString()
+    .notEmpty()
+    .withMessage("'name' query parameter must not be empty"),
 ];
 
 @autoInjectable()
@@ -28,33 +34,48 @@ export default class TileSetController {
   }
 
   routes(): Router {
-    this.router.get('/', async (req: Request, res: Response) => {
-      const tileSets = await this.tileSetService.findAll();
-      return res.status(200).json(tileSets);
-    });
+    this.router.get(
+      '/',
+      async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const tileSets = await this.tileSetService.findAll();
+          return res.status(200).json(tileSets);
+        } catch (error: unknown) {
+          if (error instanceof Error) {
+            return next(error);
+          }
+        }
+      }
+    );
 
     this.router.get(
       '/name/:name',
       validateName,
-      async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({
-            message: errors.array()[0].msg,
-          });
-        }
+      async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const errors = validationResult(req);
 
-        const {name} = req.params;
-        const tileSet = await this.tileSetService.findByName(
-          name.toLowerCase()
-        );
+          if (!errors.isEmpty()) {
+            return res.status(400).json({
+              message: errors.array()[0].msg,
+            });
+          }
+          const {name} = req.params;
+          const tileSet = await this.tileSetService.findByName(
+            name.toUpperCase()
+          );
 
-        if (tileSet) {
-          return res.status(200).json(tileSet);
-        } else {
-          return res.status(404).json({
-            message: `No Scrabble tile set found with name '${name}'.`,
-          });
+          if (tileSet) {
+            return res.status(200).json(tileSet);
+          } else {
+            return res.status(404).json({
+              message: `No Scrabble tile set found with name '${name}'.`,
+            });
+          }
+        } catch (error: unknown) {
+          if (error instanceof Error) {
+            return next(error);
+          }
         }
       }
     );
@@ -62,20 +83,27 @@ export default class TileSetController {
     this.router.get(
       '/language/:language',
       validateLanguage,
-      async (req: Request, res: Response) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({
-            message: errors.array()[0].msg,
-          });
+      async (req: Request, res: Response, next: NextFunction) => {
+        try {
+          const errors = validationResult(req);
+
+          if (!errors.isEmpty()) {
+            return res.status(400).json({
+              message: errors.array()[0].msg,
+            });
+          }
+
+          const {language} = req.params;
+          const tileSets = await this.tileSetService.findByLanguage(
+            language.toLowerCase()
+          );
+
+          return res.status(200).json(tileSets);
+        } catch (error: unknown) {
+          if (error instanceof Error) {
+            return next(error);
+          }
         }
-
-        const {language} = req.params;
-        const tileSets = await this.tileSetService.findByLanguage(
-          language.toLowerCase()
-        );
-
-        return res.status(200).json(tileSets);
       }
     );
 
